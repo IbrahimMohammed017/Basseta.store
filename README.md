50406106cbf1932a3b7d2c4c420461a672107708
Ibrahim Mohammed <56862776+IbrahimMohammed017@users.noreply.github.com>
2024-06-14 19:27:14 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Basseta.store" 
---
+++ js/main.js
@@ -0,0 +1,9 @@
+// /********** Trigger Nice Scroll **********/
+// $(document).ready(function () {
+//   $("html").niceScroll({
+//     zindex: 1000,
+//     cursorwidth: "7.5px",
+//     cursorcolor: "#ff4800",
+//     cursorborder: "1px solid #ff6200",
+//   });
+// });
