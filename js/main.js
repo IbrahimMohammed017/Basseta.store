c77abc6aa08aa8abfdfb68ea1bf811f832b6678e
Ibrahim Mohammed <56862776+IbrahimMohammed017@users.noreply.github.com>
2024-06-17 18:33:41 +0300

    new edit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 /********** Products Card Filter **********/
 document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".filter-nav .nav-link");
+  const buttons = document.querySelectorAll(
+    ".navbar-collapse .nav-link"
+  );
   const items = document.querySelectorAll(".product .item");
 
   buttons.forEach((li) => {
@@ -50,4 +52,21 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     return array;
   }
+});
+
+/********** Search Bar **********/
+document.addEventListener("DOMContentLoaded", () => {
+  let search = document.getElementById("searchInput");
+  search.addEventListener("keyup", () => {
+    let searchValue = search.value.trim().toUpperCase();
+    let keySearch = document.querySelectorAll(".card");
+    for (let i = 0; i < keySearch.length; i++) {
+      let div = keySearch[i];
+      if (div.textContent.trim().toUpperCase().indexOf(searchValue) == -1) {
+        div.parentElement.style.display = "none";
+      } else {
+        div.parentElement.style.display = "block";
+      }
+    }
+  });
 });
